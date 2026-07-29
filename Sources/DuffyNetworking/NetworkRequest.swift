c38212320c5bc8178906582a2804bcc5cd500637
Joseph Duffy <commits@josephduffy.co.uk>
2026-07-28 20:00:16 -0700

    Support setting request headers

--- Sources/DuffyNetworking/NetworkRequest.swift
+++ Sources/DuffyNetworking/NetworkRequest.swift
@@ -628,3 +628,28 @@ extension HTTPRequestConfiguration {
         )
     }
 }
+
+public struct HeaderRequestModifier: HTTPRequestModifier {
+    private let name: HTTPField.Name
+    private let value: String
+
+    public init(name: HTTPField.Name, value: String) {
+        self.name = name
+        self.value = value
+    }
+
+    public func modifyRequest(_ request: inout HTTPRequest, body: inout Data?) {
+        request.headerFields[name] = value
+    }
+}
+
+extension HTTPRequestConfiguration {
+    public func header(
+        name: HTTPField.Name,
+        value: String,
+    ) -> Self {
+        requestModifier(
+            HeaderRequestModifier(name: name, value: value)
+        )
+    }
+}
