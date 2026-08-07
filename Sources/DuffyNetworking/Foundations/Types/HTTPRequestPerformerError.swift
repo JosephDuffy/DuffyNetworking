39a0380bea09db5f684cc58a011795ef787ac25c
Joseph Duffy <commits@josephduffy.co.uk>
2026-08-06 19:21:22 -0700

    Expose underlying error’s description

--- Sources/DuffyNetworking/Foundations/Types/HTTPRequestPerformerError.swift
+++ Sources/DuffyNetworking/Foundations/Types/HTTPRequestPerformerError.swift
@@ -1,4 +1,10 @@
-public struct HTTPRequestPerformerError<ResponseBody: Sendable>: Error {
+#if canImport(FoundationEssentials)
+import FoundationEssentials
+#else
+import Foundation
+#endif
+
+public struct HTTPRequestPerformerError<ResponseBody: Sendable>: LocalizedError {
     public let underlyingError: any Error
 
     public let requestConfiguration: HTTPRequestConfiguration<ResponseBody>
@@ -6,4 +12,12 @@ public struct HTTPRequestPerformerError<ResponseBody: Sendable>: Error {
     public let request: HTTPRequestSnapshot?
 
     public let response: HTTPResponseSnapshot?
+
+    public var errorDescription: String? {
+        if let underlyingError = underlyingError as? LocalizedError {
+            return underlyingError.errorDescription
+        } else {
+            return underlyingError.localizedDescription
+        }
+    }
 }
