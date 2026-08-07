3beda84b0e3d3e3d5c0ec59dfd8a0a62f560775d
Joseph Duffy <commits@josephduffy.co.uk>
2026-08-06 19:25:02 -0700

    Conform ResponseValidatorErrors to LocalizedError

--- Sources/DuffyNetworking/Foundations/Types/ResponseValidatorErrors.swift
+++ Sources/DuffyNetworking/Foundations/Types/ResponseValidatorErrors.swift
@@ -1,6 +1,29 @@
+#if canImport(FoundationEssentials)
+import FoundationEssentials
+#else
+import Foundation
+#endif
+
 /// The errors produced by response validators, in validator execution order.
-public struct ResponseValidatorErrors: Error {
+public struct ResponseValidatorErrors: LocalizedError {
     public let errors: [any Error]
+
+    public var errorDescription: String? {
+        let descriptions = errors
+            .compactMap { error in
+                if let error = error as? LocalizedError {
+                    return error.errorDescription
+                } else {
+                    return error.localizedDescription
+                }
+            }
+
+        if descriptions.isEmpty {
+            return nil
+        } else {
+            return descriptions.joined(separator: "\n")
+        }
+    }
 
     internal init(errors: [any Error]) {
         precondition(!errors.isEmpty)
